43d8efee66d74d3597e225c255565ea9ccadbb89
ArthurCorreia213 <arthurcnascimento132@gmail.com>
2024-11-27 16:51:33 -0300

    Conexão ao banco de dados concluida, adicionado formulario de registro de empresas
    Co-authored-by: Mateus Santos <Matt-ags@users.noreply.github.com>
    Co-authored-by: Gabriel <GBTorto@users.noreply.github.com>

--- app.js
+++ app.js
@@ -4,28 +4,28 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-// const dbConfig = {
-    // user: 'user',
-    // password: 'password',
-    // server: '127.0.0.1',
-    // database: 'db',
-    // options: {
-        // encrypt: true,
-        // trustServerCertificate: true
-    // }
-// };
+const dbConfig = {
+    user: 'usuario',
+    password: 'senai103103',
+    server: '127.0.0.1',
+    database: 'Estagios',
+    options: {
+        encrypt: true,
+        trustServerCertificate: true
+    }
+};
 
-// Rota para buscar vagas
-// app.get('/api/vagas', async (req, res) => {
-    // try {
-        // const pool = await sql.connect(dbConfig);
-        // const result = await pool.request().query('SELECT * FROM Vagas');
-        // res.json(result.recordset); // Retorna as vagas no formato JSON
-    // } catch (err) {
-        // console.error(err);
-        // res.status(500).send('Erro ao buscar vagas.');
-    // }
-// })
+// Rota para buscar empresas
+app.get('/api/empresas', async (req, res) => {
+    try {
+        const pool = await sql.connect(dbConfig);
+        const result = await pool.request().query('SELECT * FROM Empresas');
+        res.json(result.recordset); // Retorna as empresas no formato JSON
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Erro ao buscar vagas.');
+    }
+})
 
 app.use(cors());
 app.use(express.json());
@@ -33,4 +33,36 @@ app.use(express.static('public')); // Serve arquivos estáticos da pasta 'public
 
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
+});
+
+sql.connect(dbConfig)
+    .then(pool => {
+        console.log('Conexão com o banco de dados estabelecida!');
+        return pool; // Retorna a pool de conexões
+    })
+    .catch(err => {
+        console.error('Erro ao conectar ao banco de dados:', err);
+    });
+
+app.post('/cadastro', async (req, res) => {
+    const { nome, cnpj, contato, status_cadastro } = req.body;
+
+    try {
+        await sql.connect(dbConfig);
+        const query = `
+            INSERT INTO Empresas (Nome, CNPJ, Contato, Status_Cadastro)
+            VALUES (@Nome, @CNPJ, @Contato, @Status_Cadastro)
+        `;
+
+        const request = new sql.Request();
+        request.input('nome', sql.NVarChar, nome);
+        request.input('cnpj', sql.NVarChar, cnpj);
+        request.input('contato', sql.NVarChar, contato);
+        request.input('status_cadastro', sql.NVarChar, status_cadastro);
+        await request.query(query);
+
+        res.send('Produto adicionado com sucesso!');
+    } catch (error) {
+        res.status(500).send('Erro ao adicionar produto: ' + error.message);
+    }
 });
