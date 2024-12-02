0bca3ceb491729f5d7ce6b2cc7b479dbb18ab79a
ArthurCorreia213 <arthurcnascimento132@gmail.com>
2024-12-02 16:51:18 -0300

    Tela de adição de vaga por parte da empresa alterada
    Co-authored-by: Gabriel <GBTorto@users.noreply.github.com>
    Co-authored-by: Mateus Santos <Matt-ags@users.noreply.github.com>

--- app.js
+++ app.js
@@ -27,6 +27,28 @@ app.get('/api/empresas', async (req, res) => {
     }
 })
 
+app.get('/api/empresas-pendentes', async (req, res) => {
+    try {
+        const pool = await sql.connect(dbConfig);
+        const result = await pool.request().query("SELECT * FROM Empresas WHERE Status_Cadastro = 'Pendente'");
+        res.json(result.recordset); // Retorna as empresas no formato JSON
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Erro ao buscar vagas.');
+    }
+})
+
+app.get('/api/candidatos', async (req, res) => {
+    try {
+        const pool = await sql.connect(dbConfig);
+        const result = await pool.request().query('SELECT * FROM Candidatos');
+        res.json(result.recordset); // Retorna os alunos no formato JSON
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Erro ao buscar vagas.');
+    }
+})
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public')); // Serve arquivos estáticos da pasta 'public'
@@ -66,3 +88,63 @@ app.post('/cadastro', async (req, res) => {
         res.status(500).send('Erro ao adicionar produto: ' + error.message);
     }
 });
+
+app.post('/add_vaga', async (req, res) => {
+    const { nome, contato, vagas, salario, tempo_de_contrato, horario, beneficios, requisitos } = req.body;
+
+    try {
+        await sql.connect(dbConfig);
+        const query = `
+            INSERT INTO Vagas (Nome, Contato, Vagas, Salario, Tempo_De_Contrato, Horario, Beneficios, Requisitos)
+            VALUES (@Nome, @Contato, @Vagas, @Salario, @Tempo_De_Contrato, @Horario, @Beneficios, @Requisitos)
+        `;
+
+        const request = new sql.Request();
+        request.input('nome', sql.NVarChar, nome);
+        request.input('contato', sql.NVarChar, contato);
+        request.input('vagas', sql.Int, vagas);
+        request.input('salario', sql.Float, salario);
+        request.input('tempo_de_contrato', sql.NVarChar, tempo_de_contrato);
+        request.input('horario', sql.NVarChar, horario);
+        request.input('beneficios', sql.NVarChar, beneficios);
+        request.input('requisitos', sql.NVarChar, requisitos);
+        await request.query(query);
+
+        res.send('Produto adicionado com sucesso!');
+    } catch (error) {
+        res.status(500).send('Erro ao adicionar produto: ' + error.message);
+    }
+});
+
+
+
+
+
+
+app.put('/api/aprovar/:id', async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      // Conectar ao banco de dados
+      const pool = await sql.connect(dbConfig);
+  
+      // Executar a query de atualização
+      const result = await pool
+        .request()
+        .input('id', sql.Int, id)
+        .query('UPDATE Empresa SET Status_Cadastro = @status WHERE id = @id', {
+          status: 'Aprovado',
+        });
+  
+      if (result.rowsAffected[0] > 0) {
+        res.status(200).json({ message: 'Registro atualizado com sucesso', status: 'Aprovado' });
+      } else {
+        res.status(404).json({ message: 'Registro não encontrado' });
+      }
+    } catch (error) {
+      console.error('Erro ao atualizar registro:', error);
+      res.status(500).json({ message: 'Erro ao atualizar registro' });
+    } finally {
+      sql.close();
+    }
+  });
